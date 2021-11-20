0ca38390afdc6a7c7a7b0be1dc027cf47c709a9c
Alveiro Garcia <albeirog82@gmail.com>
2021-11-20 18:06:03 -0500

    punto 1 y 2 microdatos

--- microdatos.R
+++ microdatos.R
@@ -1,2 +1,14 @@
 library(sdcMicro)
 
+data(francdat)
+x <- francdat [,c(2,4,5,6,8)]
+print(x)
+
+ff <- freqCalc(x, keyVars=c(1,2,3,4) , w=5)
+print(cbind(x,ff$fk,ff$Fk))
+
+##################################################
+
+rk <- indivRisk(ff)$rk
+print (cbind(x, ff$fk, ff$Fk, rk ))
+
